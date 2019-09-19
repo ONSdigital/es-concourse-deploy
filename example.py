642235dd19a802e2e9fd26ec9eae100efd452cfa
“thomashenson” <thomasjameshenson@gmail.com>
2019-09-19 10:54:44 +0100

    Changed git repo and fixed flake8 issues.

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ def lambda_handler(event, context):
     world = event
     print(world)
 
+
 def exampleMethod():
     badExample = 0
     return badExample
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,9 @@
+import example
+
 import sys
 import os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
-import example
+
 
 def test_example():
     assert example.exampleMethod() == 0
