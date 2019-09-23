c9416e39bea694f8b12b067c6c9f65e75997d381
Jordancooke <Jordanleecooke@gmail.com>
2019-09-23 16:39:49 +0100

    Refactor

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,12 @@
-def test_blank():
-    print("hello_world!")
+import sys
+import os
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+import example
+
+def test_example():
+    assert example.example_method() == 0
+
+# Working Example
+# def test_blank():
+#     print("hello_world!")
