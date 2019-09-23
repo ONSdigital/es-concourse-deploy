68bb81e811e9d58835d877a2e80bf7155b91e9d6
Jordancooke <Jordanleecooke@gmail.com>
2019-09-23 16:44:37 +0100

    Refactor

--- flake8.sh
+++ flake8.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# The script will exit when an error is encountered
-#set -e
-
 # Run flake8 linting
-flake8 repository #&& echo "flake8 passed. Yayyy." || exit 1
+flake8 repository
