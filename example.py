9d9e4b095e65147ce12730bf4336c4dd3c04c655
Jordancooke <Jordanleecooke@gmail.com>
2019-09-30 17:45:49 +0100

    Removing intentional test failures and linting errors inside example and test_example, updating pipeline to use base python image

--- example.py
+++ example.py
@@ -1,8 +1,2 @@
-import sys
-import os
-
-sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
-import example
-
-def test_example():
-    assert example.example_method() == 0
+def example_method():
+    return 0
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
-import example
-
-def test_example():
-    assert example.example_method() == 0
-
-# Working Example
-# def test_blank():
-#     print("hello_world!")
+#Working Example
+def test_blank():
+    print("hello_world!")
